2641dff5dae3b4623757229a32640df32b7de3d9
Synimistic <47408727+Synimistic@users.noreply.github.com>
2020-02-07 19:07:49 -0500

    Update Loader.lua

--- Syn Admin Commands/Setup/Loader.lua
+++ Syn Admin Commands/Setup/Loader.lua
@@ -1,44 +1,13 @@
 --// Syn Admin Commands; Loader \\--
 
---<< Variables >>--
-
--- Syn Setup
+--<< Setup >>--
 local Folder = script.Parent.Parent.Parent
 local Setup = Folder:FindFirstChild('Setup')
 local Plugins = Setup:FindFirstChild('Plugins')
 local settings = Setup:FindFirstChild('Settings')
 
 -- MainModule; https://www.roblox.com/library/4665853426/Syn-Admin-Commands-MainModule
-local MainModule = game:GetService('ReplicatedStorage').MainModule
-
---<< Setup Check >>--
-
--- Setup  Folder
-assert(Setup, 'Syn: Setup folder not found!')
-
--- settings Module
-assert(settings, 'Syn: Settings module not found!')
-
--- Plugins Folder
-if not Plugins then
-	warn('Syn: Plugins not found!')
-	Plugins = Instance.new('Folder', Setup)
-	Plugins.Name = 'Plugins'
-end
-
--- settings
-if type(settings) ~= 'table' then
-	-- Check if settings is ModuleScript
-	pcall(function()
-		if settings:IsA('ModuleScript') then
-			settings = require(settings)
-		end
-	end)
-	-- Final settings check.
-	if type(settings) ~= 'table' then
-		error('Syn: Error fetching settings.')
-	end
-end
+local MainModule = game:GetService('ReplicatedStorage'):FindFirstChild('MainModule')
 
 --<< Run MainModule >>--
 
