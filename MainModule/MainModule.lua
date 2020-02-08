343c087c6427ccf75b8139b106bdfd02135d84b4
Synimistic <47408727+Synimistic@users.noreply.github.com>
2020-02-07 19:08:51 -0500

    Update MainModule.lua

--- MainModule/MainModule.lua
+++ MainModule/MainModule.lua
@@ -2,4 +2,43 @@
 
 return function(Data)
 	
+	--<< Setup >>--
+	local Folder = Data.Folder
+	local Setup = Folder:FindFirstChild('Setup')
+	local Plugins = Setup:FindFirstChild('Plugins')
+	local settings = Setup:FindFirstChild('Settings')
+	
+	--<< Setup Check >>--
+	
+	-- Setup Folder
+	assert(Setup, 'Syn: Setup folder not found!')
+	
+	-- settings Module
+	assert(settings, 'Syn: Settings module not found!')
+	
+	-- Plugins Folder
+	if not Plugins then
+		warn('Syn: Plugins not found!')
+		Plugins = Instance.new('Folder', Setup)
+	end
+	Plugins.Name = 'Plugins'
+	
+	-- settings
+	if type(settings) ~= 'table' then
+		-- Check if settings is ModuleScript
+		pcall(function()
+			if settings:IsA('ModuleScript') then
+				settings = require(settings)
+			end
+		end)
+	end
+	
+	-- Final settings check.
+	if type(settings) ~= 'table' then
+		error('Syn: Error fetching settings.')
+	end
+	
+	
+	
+	warn('Syn: Successfully loaded MainModule.')
 end
